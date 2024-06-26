491a0b5fe47d75c5e2af197e24d7e02a56245d0f
j-princi3 <jainprinci00@gmail.com>
2024-06-26 16:26:14 +0530

    sub domain added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Start from './screens/Start';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ShowDesign from './screens/ShowDesigns';
-// import DragShapes from './screens/DragShapes';
 import NotFound from './screens/NotFound';
 import DesignStructure from './screens/DesignStructure';
 import AllShapes from './screens/AllShapes';
-// import Mainmenu from './screens/Mainmenu';
+
 export default function App() {
   return (
-    <div >
-      <Router>
-          <Routes>
-            <Route path="/" element={<Start/>} />
-            <Route path="/showdesign/:id" element={<ShowDesign/>} />
-            {/* <Route path="/dragshapes/:id" element={<DragShapes/>}/> */}
-            <Route path="*" element={<NotFound />} />
-            <Route path="/allshapes" element={<AllShapes />} />
-            <Route path="/designstructure/:id" element={<DesignStructure />} />
-            {/* <Route path="/mainmenu" element={<Mainmenu/>} /> */}
-          </Routes>
-      </Router>
-      
+    <div>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/showdesign/:id" element={<ShowDesign />} />
+        <Route path="/allshapes" element={<AllShapes />} />
+        <Route path="/designstructure/:id" element={<DesignStructure />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
